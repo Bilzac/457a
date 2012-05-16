62f381018633f94c06fc8254eafc61339d14deb4
Pooja Sardesai <pmsardes@gmail.com>
2012-05-16 14:22:17 -0400

    Implemented GamePiece (basic)

--- Assignment457/Assignment457/Assignment1b.cs
+++ Assignment457/Assignment457/Assignment1b.cs
@@ -8,7 +8,6 @@ namespace Assignment457
     class Assignment1b
     {
 
-
-
+       
     }
 }
--- Assignment457/Assignment457/GamePiece.cs
+++ Assignment457/Assignment457/GamePiece.cs
@@ -7,8 +7,38 @@ namespace Assignment457
 {
     //The object that stores the colour and number of pieces in each square
     //black, white, null; number = 0 to 10
+
+    public enum Colour
+    {
+        WHITE,
+        BLACK,
+        NONE
+    }
+
+    // a square on the board
     class GamePiece
     {
+        // private variables
+        private Colour _colour;
+        private byte _stones;
 
+        // Public getters and setters
+        public Colour colour
+        {
+            set { this._colour = value; }
+            get { return this._colour; }
+        }
+        public byte stones
+        {
+            set { this._stones = value; }
+            get { return this._stones; }
+        }
+
+         //default constructor 
+        public GamePiece(Colour colour, byte stones)
+        {
+            _colour = colour;
+            _stones = stones;
+        }
     }
 }
