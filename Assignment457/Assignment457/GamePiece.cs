1464088b1eff8bd3d92f4ca778e2258cbb0952eb
Shweta Aladi <shweta.aladi@gmail.com>
2012-05-16 11:04:25 -0400

    Adding skeleton files for part b

---
+++ Assignment457/Assignment457/GamePiece.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assignment457
+{
+    //The object that stores the colour and number of pieces in each square
+    class GamePiece
+    {
+    }
+}
