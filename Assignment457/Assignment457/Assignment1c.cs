912e6030124f544e14f2d9077e23ad1e35c92bf4
Pooja Sardesai <pmsardes@gmail.com>
2012-05-26 00:11:28 -0400

    Created basic functions of Tabu search - still very incomplete. Added Pair.cs (keeps track of swapped values) .

--- Assignment457/Assignment457/Assignment1c.cs
+++ Assignment457/Assignment457/Assignment1c.cs
@@ -66,7 +66,131 @@ namespace Assignment457
                 {0,  1,  5, 0,  5, 1,  5, 10, 10, 2, 2,  5,  5,  5,  0, 10,  0, 0, 1,  6,  0}
             };
 
+            doTabuSearch(distanceMatrix, flowMatrix);
+
             return; 
+        }
+
+        public static void doTabuSearch(int [,] distanceMatrix, int[,] flowMatrix)
+        {
+            // department[i] = department, i = location
+            int[] departments = new int[21] 
+                {  
+                   0, // department does not exist
+                   1,  2,  3,  4,  5,  6,  7,  8,  9, 10,
+                  11, 12, 13, 14, 15, 16, 17, 18, 19, 20 
+                };
+
+            // get initial solution 
+            int currentSolution = computeSolution(departments, distanceMatrix, flowMatrix);
+
+            // set initial solution as the best solution
+            int bestSolution = currentSolution;
+
+            // for testing purposes
+            Console.WriteLine(currentSolution);
+
+            // create tabu list
+            List<Pair> tabuList = new List<Pair>();
+
+            // find optimal solution
+            int counter = 0;
+            while (counter < 20) // for now, stop after three iterations
+            {
+                counter++; // for testing purposes only....
+
+                resetTabuList(tabuList);
+
+                // Get sorted candidate list
+                List<Pair> validPairs = getCandidatesFromNeighbourhood(tabuList, departments, distanceMatrix, flowMatrix);
+                Pair bestCandidate = validPairs.ElementAt(0);
+                
+                // check to see if the new solution is optimal than the previous solution
+                if (bestCandidate.solution < currentSolution)
+                {
+                    currentSolution = bestCandidate.solution;
+                    // save ordering
+                    Console.WriteLine(bestCandidate.x + " " + bestCandidate.y);
+                    departments = swapValues(departments, bestCandidate.x, bestCandidate.y);
+                    bestCandidate.tenure = 4;
+                    tabuList.Add(bestCandidate);
+                }
+            }
+
+            Console.WriteLine(currentSolution);
+        }
+
+        public static void resetTabuList(List<Pair> tabuList)
+        {
+            // subtract tenure by 1
+            tabuList.Select(c => { c.tenure = c.tenure - 1; return c; }).ToList();
+            // remove all pairs with tenure = 0
+            var query = from pair in tabuList
+                        where pair.tenure != 0
+                        select pair;
+            tabuList = query.ToList();
+        }
+
+        public static List<Pair> getCandidatesFromNeighbourhood(List<Pair> tabuList, int[] departments, int[,] distanceMatrix, int[,] flowMatrix)
+        {
+            List<Pair> list = new List<Pair>();
+            for (int i = 1; i < departments.Length; i++)
+            {
+                for (int j = 1; j < departments.Length; j++)
+                {
+                    int[] newDepartments = swapValues(departments, i, j);
+                    int solution = computeSolution(newDepartments, distanceMatrix, flowMatrix);
+                    Pair pair = new Pair(i, j, solution);
+
+                    // if not in tabu list, add to candidate list
+                    var tabuQuery = from tabuPair in tabuList
+                                    where tabuPair.x == i && tabuPair.y == j
+                                    select tabuPair;
+                    if (tabuQuery.ToList().Count == 0)
+                    {
+                        list.Add(pair);
+                    }
+                }
+            }
+
+            // order list with the optimal solution first
+            var query = from pair in list
+                        orderby pair.solution ascending
+                        select pair;
+            return query.ToList();
+        }
+
+        public static int[] swapValues(int[] list, int x, int y)
+        {
+            // create a new list
+            int[] newList = new int[21];
+            for (int i = 0; i < newList.Length; i++)
+            {
+                newList[i] = list[i];
+            }
+
+            // swap values in the array
+            int a = newList[x], b = newList[y];
+            newList[y] = a;
+            newList[x] = b;
+            return newList;
+        }
+
+        public static int computeSolution(int[] departments, int[,] distanceMatrix, int[,] flowMatrix)
+        {
+            // find initial solution
+            int solution = 0;
+            for (int i = 1; i < departments.Length; i++)
+            {
+                for (int j = 1; j < departments.Length; j++)
+                {
+                    int flow = flowMatrix[departments[i], departments[j]];
+                    int distance = distanceMatrix[i, j];
+                    solution = solution + (distance * flow);
+                }
+            }
+
+            return solution;
         }
     }
 }
---
+++ Assignment457/Assignment457/Pair.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assignment457
+{
+    class Pair
+    {
+        // Private variables
+        int _x;
+        int _y;
+        int _solution;
+        int _tenure;
+
+        // Public getters
+        public int x 
+        {
+            get { return this._x; }
+        }
+        public int y 
+        {
+            get { return this._y; }
+        }
+        public int solution 
+        {
+            get { return this._solution; }
+        }
+        public int tenure 
+        {
+            set { this._tenure = value; } 
+            get { return this._tenure; }
+        }
+
+        // Constructor
+        public Pair(int x, int y, int solution)
+        {
+            _x = x;
+            _y = y;
+            _solution = solution;
+        }
+    }
+}
