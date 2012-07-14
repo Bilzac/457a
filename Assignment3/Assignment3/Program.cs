1ed99b1c15bcb4ea0b559d1818154a2c1422339c
Shweta Aladi <shweta.aladi@gmail.com>
2012-07-14 00:35:13 -0400

    Adding console selection for each question

--- Assignment3/Assignment3/Program.cs
+++ Assignment3/Assignment3/Program.cs
@@ -9,20 +9,44 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Part 1B: ");
-            
-            for (int i = 0; i < 10; i++)
-            {
-                Part1b b = new Part1b(30, 1000, 0.5, 0.5);
-                b.RunParticleSwarmOptimization();
-
-            }
             while (true)
-            {    
+            {
+                //Question1 q1 = null;
+                Part1b b = null;
+                //Question3 q3 = null;
 
+                Console.WriteLine("===============================================================");
+                Console.WriteLine("ECE457A - ASSIGNMENT 3");
+                Console.WriteLine("Shweta Aladi, Bilal Arshad, Pooja Sardesai");
+                Console.WriteLine("===============================================================\n");
+                Console.WriteLine("Select the assignment question:");
+                Console.WriteLine("[A] Camelback Function (PSO)");
+                Console.WriteLine("[B] Sphere Function (PSO)");
+                Console.WriteLine("[C] Travelling Salesman Problem (ACO)");
+                Console.WriteLine("[Q] Quit");
+                char a = Console.ReadLine().ToLower().ToCharArray()[0];
+
+                switch (a)
+                {
+                    case 'a':
+
+                        break;
+                    case 'b':
+                        b = new Part1b(30, 1000, 0.5, 0.5);
+                        b.RunParticleSwarmOptimization();
+                        break;
+                    case 'c':
+
+
+                        break;
+                    case 'q':
+                        Console.WriteLine("Exiting...");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid selection");
+                        break;
+                }
             }
-
         }
     }
 }
